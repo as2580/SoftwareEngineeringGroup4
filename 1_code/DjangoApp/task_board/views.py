2194c436d24d93c1f8e046a332618ae47008c98b
Kimberly C <kc875@scarletmail.rutgers.edu>
2020-03-28 20:30:58 -0400

    Fixed Typos in Task Board App

--- 1_code/DjangoApp/task_board/views.py
+++ 1_code/DjangoApp/task_board/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import *
 
+import task_board.task_manager_db_helper as tm_db
+
 def index(request):
     tasks = tm_db.get_all_tasks()
     context = {'tasks': tasks}
-    return render(request, 'task_manager/index.html', context)
+    return render(request, 'task_board/index.html', context)
