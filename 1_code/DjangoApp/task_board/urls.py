16f9f640e60a97b1b5bf91d3eb0290f473aad1a6
Kimberly C <kc875@scarletmail.rutgers.edu>
2020-03-28 22:53:35 -0400

    Added more URLs for Task Board

--- 1_code/DjangoApp/task_board/urls.py
+++ 1_code/DjangoApp/task_board/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('claim_tasks', views.edit, name='claim_tasks'),
-	path('view_tasks', views.modify, name='view_tasks'),
+    path('claim_tasks', views.claim_tasks, name='claim_tasks'),
+	path('view_tasks', views.view_tasks, name='view_tasks'),
+	path('claim', views.claim, name='claim'),
+	path('complete', views.complete, name='complete'),
 ]
