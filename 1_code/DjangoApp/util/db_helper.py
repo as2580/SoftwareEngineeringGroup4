bbab20f263527d2f20dc27bc38b2f34f7dce2c5b
Andrew S <as2580@rutgers.edu>
2020-03-30 01:37:18 -0400

    db_helper fix

--- 1_code/DjangoApp/util/db_helper.py
+++ 1_code/DjangoApp/util/db_helper.py
@@ -182,7 +182,8 @@ def modify_task(taskID, taskName=None, description=None, state=None, employeeID=
 def get_item_info(RFID):
 	q = "select name, price from SE_DB.items where RFID = "+RFID+";"
 	c = db_util.db_open()
-	info = db_util.db_execute(c, q)
+	print(q)
+	info = db_util.db_query(c, q)
 	db_util.db_close(c)
 	return info
 
