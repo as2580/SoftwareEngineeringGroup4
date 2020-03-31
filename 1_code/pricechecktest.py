e4f5151529364f06ad402b626ba92d4206db0c65
AmaliJD <60673543+AmaliJD@users.noreply.github.com>
2020-03-31 17:01:35 -0400

    Add files via upload

--- 1_code/pricechecktest.py
+++ 1_code/pricechecktest.py
@@ -1,5 +1,12 @@
+# NOTE:
+# This is just a demo. Features such as the RFID scanner and the Items Database
+# have not yet been implemented. Thus, they are being simulated through an
+# input text field (rfid scanner) and a pre-made list of Items (database)
+# Additionally, the methods used to create a user interface will likely not be used in the final version
+
 import tkinter as tk
 
+#setting up UI
 root = tk.Tk()
 canvas1 = tk.Canvas(root, width=400, height=300)
 canvas1.pack()
@@ -16,7 +23,7 @@ class Item:
         self.price = price
 
 
-# getprice requests from database an item with the given rfid
+# getprice requests from 'database' an item with the given rfid
 def getprice():
     i = entry1.get()
     price = -1
@@ -28,9 +35,10 @@ def getprice():
         label1 = tk.Label(root, text='                The price of this item is $' + str(price) + '                ')
     else:
         label1 = tk.Label(root, text='Error detecting item. Please scan again')
+
     canvas1.create_window(200, 230, window=label1)
 
-# input field stuff
+# input field polish
 def on_entry_click(event):
     """function that gets called whenever entry is clicked"""
     if entry1.get() == 'RFID scanner':
@@ -49,6 +57,8 @@ entry1.bind('<FocusOut>', on_focusout)
 button1 = tk.Button(text='Scan', command=getprice)
 canvas1.create_window(200, 180, window=button1)
 
+
+
 I = [] #database
 I.append(Item('aaaa2', 10.99))
 I.append(Item('aaaaa', 6.25))
@@ -60,6 +70,7 @@ I.append(Item('lo6dk', 630.00))
 I.append(Item('11111', 1.00))
 I.append(Item('22222', 2.22))
 I.append(Item('33333', 3.03))
+I.append(Item('55555', 5.50))
 I.append(Item('99999', 99.99))
 I.append(Item('rfid0', 12000.00))
 I.append(Item('12345', 15.00))
@@ -68,4 +79,6 @@ I.append(Item('00000', 100.00))
 # message to instruct user
 label1 = tk.Label(root, text='Scan an item to check its price!')
 canvas1.create_window(200, 230, window=label1)
+
+# display ui
 root.mainloop()
