da5c14561b2982477d74d89f5daa23e1a5a117ea
Abhinandan Vellanki <35554825+AbhinandanVellanki@users.noreply.github.com>
2020-04-24 16:50:18 -0400

    Update urls.py

--- 1_code/DjangoApp/login/urls.py
+++ 1_code/DjangoApp/login/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'login'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.register, name='register'),
+    path('', views.success, name='success'),
+    path('', views.login, name='login'),
 ]
