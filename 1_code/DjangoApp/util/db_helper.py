5ea5e9c87ddaa0829cdc40122858d492c0a48be1
Andrew S <as2580@rutgers.edu>
2020-04-25 15:32:09 -0400

    Update db_helper.py
    small change to get_items()

--- 1_code/DjangoApp/util/db_helper.py
+++ 1_code/DjangoApp/util/db_helper.py
@@ -255,7 +255,7 @@ def get_item_info(RFID):
 # return value:
 #	
 def get_items():
-	q = "SELECT * FROM SE_DB.items"
+	q = "SELECT name, brand, price FROM SE_DB.items"
 	c = db_util.db_open()
 	info = db_util.db_query(c, q)
 	db_util.db_close(c)
