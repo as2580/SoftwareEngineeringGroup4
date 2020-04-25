934f5c039e75ee3bde03615998c7fe7a65a1ae2a
AmaliJD <60673543+AmaliJD@users.noreply.github.com>
2020-04-25 18:03:48 -0400

    Add files via upload

--- 1_code/DjangoApp/price_checker/models.py
+++ 1_code/DjangoApp/price_checker/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models import DecimalField
+from decimal import Decimal
 
 # Create your models here.
+
+
+class Item(models.Model):
+    name = models.TextField()
+    #price = models.FloatField()
+    price = DecimalField(max_digits=20, decimal_places=2, default=Decimal(0.00))
+    rfid = models.FloatField()
+    image = models.FilePathField(path="/img")
+
+    def __init__(self, name, rfid, price, img):
+        self.name = name
+        self.rfid = rfid
+        self.price = price
+        self.image = img
+
+    def get_price(self):
+        return self.price
+
+    def get_name(self):
+        return self.name
+
+    def get_rfid(self):
+        return self.rfid
--- 1_code/DjangoApp/price_checker/views.py
+++ 1_code/DjangoApp/price_checker/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import *
+from util import db_helper
 
 # Create your views here.
 def index(request):
-    return render(request, 'price_checker/index.html')
+    RFID = "308932602"  # example rfid
+
+    request.session['rfid'] = RFID
+    item = db_helper.get_item_info(itemID)
+
+    request.session['price'] = float(item.price)
+    price = request.session['price']
+
+    request.session['name'] = item.name
+    name = request.session['name']
+
+    context = {'name': name, 'price': price, }
+    return render(request, 'price_checker/index.html', context)
