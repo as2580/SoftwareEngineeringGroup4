2497599418e626e9e52e6490c8524fa36e6a7518
AmaliJD <60673543+AmaliJD@users.noreply.github.com>
2020-04-25 22:00:46 -0400

    price_checker complete

--- 1_code/DjangoApp/price_checker/urls.py
+++ 1_code/DjangoApp/price_checker/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'price_checker'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='price_checker_url'),
+    path('index', views.index, name='price_checker_url'),
+    path('search_rfid', views.search_rfid, name='price_checker_url'),
 ]
--- 1_code/DjangoApp/price_checker/views.py
+++ 1_code/DjangoApp/price_checker/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import *
+from util import db_helper
 
 # Create your views here.
 def index(request):
     return render(request, 'price_checker/index.html')
+
+def search_rfid(request):
+    RFID = str(request.GET['rfid'])
+    if db_helper.get_item_info(RFID):
+        item = db_helper.get_item_info(RFID)
+        name = "Found Item: " + str(item[0][0])
+        price = "Price: $" + str(item[0][1])
+        context = {'name': name, 'price': price, }
+    else:
+        #name = "Error Finding Item"
+        #price = "Please Scan Again"
+        error = "ERROR FINDING ITEM\nPLEASE SCAN AGAIN"
+        context = {'error': error,}
+
+    return render(request, 'price_checker/index.html', context)
